1ca13cbb373f4db78177648288bd3b135db433d7
Jhonse02 <jhonse021220@gmail.com>
2021-01-16 22:56:59 -0500

    whyred: Include MiuiCamera

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -22,6 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# Inherit MiuiCamera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
